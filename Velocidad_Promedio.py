fe170b70c4211aedccd36dca3e6f0d825b8485cf
AndreArOMOortega <46934792+AndreArOMOortega@users.noreply.github.com>
2019-01-22 11:04:23 -0600

    Andrea Romo Ortega, grupo 03

---
+++ Velocidad_Promedio.py
@@ -0,0 +1,9 @@
+#Autor: Andrea Romo Ortega
+#Prueba: Programación. Calcular velociadad promedio
+
+distancia= int(input("Teclea la distancia de tu viaje en km enteros: "))
+tiempo = int(input("Teclea el tiempo de tu viaje en horas enteras: "))
+
+velocidad = distancia / tiempo
+
+print("Velocidad promedio:" , velocidad, "km/h")
